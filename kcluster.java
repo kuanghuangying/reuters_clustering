0c06d5027e43e9105e7456733d37d0c138f26358
Huangying Kuang <kuanghuangying@gmail.com>
2017-11-27 19:33:09 -0600

    complete evaluation

--- kcluster.java
+++ kcluster.java
@@ -26,6 +26,8 @@ public class kcluster{
     private static final int[] seeds = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39};
     static String deliminator = ",";
     static String newline = "\n";
+    static float entropy;
+    static float purity;
 
 
     private static point[] getAllPoints(){
@@ -136,6 +138,30 @@ public class kcluster{
         report.close();
     }
 
+    private static void get_entropy_purity(int[][] distribution,kmeans best_trial){
+        int totalCol = distribution[0].length;
+        int total = best_trial.points.length;
+        float e = 0;
+        float pu = 0;
+
+        for (int row = 0; row < distribution.length; row++){
+            float e_j = 0;
+            float pu_j = Float.MIN_VALUE;
+            float row_sum = (float)best_trial.clusters[row].getSize();
+            for (int col = 0; col < totalCol; col ++){
+
+                float p = distribution[row][col] / row_sum;
+                e_j += ( p *  ( Math.log(p)/Math.log(2)) );
+
+                if (distribution[row][col] > pu_j) pu_j = distribution[row][col];
+            }
+            e += (row_sum/total) * e_j;
+            pu += (row_sum/total) * pu_j;
+        }
+        entropy = e;
+        purity = pu;
+    }
+
     private static void write_evaluation(int[][] distribution, double best_criterion, long totalTime) throws IOException{
         PrintWriter report = new PrintWriter(new FileWriter("evaluation.csv",true));
         StringBuilder sb = new StringBuilder();
@@ -144,6 +170,10 @@ public class kcluster{
         sb.append(n_cluster);
         sb.append(deliminator);
         sb.append(best_criterion);
+        sb.append(deliminator);
+        sb.append(entropy);
+        sb.append(deliminator);
+        sb.append(purity);
         sb.append(deliminator);
         sb.append(totalTime);
         sb.append(newline);
@@ -231,7 +261,7 @@ public class kcluster{
         */
         long endTime = System.currentTimeMillis();
         long totalTime = endTime - startTime;
-
+        System.out.println(totalTime);
 
 
         write_output_file(best_trial);
@@ -239,7 +269,8 @@ public class kcluster{
         int[][] distribution =   get_distribution(best_trial);
         write_distribution(distribution);
 
-        write_evaluation(best_criterion,distribution,totalTime);
+        get_entropy_purity(distribution,best_trial);
+        write_evaluation(distribution,best_criterion,totalTime);
 
     }
 }
